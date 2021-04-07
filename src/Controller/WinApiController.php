bb2358197456ba7b19e1986392caba340e32bdd4
Vendeville Fabien <fabien.vendeville@inetum.world>
2021-04-07 10:11:46 +0200

    First proposition of Fabien

--- src/Controller/WinApiController.php
+++ src/Controller/WinApiController.php
@@ -2,7 +2,19 @@
 
 namespace App\Controller;
 
+/**
+ * Class WinApiController
+ * @package App\Controller
+ *  @Route("/resultats", name="results", methods={"GET"})
+ */
+
 class WinApiController
 {
     // Fournir une API qui donne le dernier résultat à l'EuroMillions
+
+    function afficheResultats() {
+        return $this->render('results.html.twig', [
+            'liste' => lireCsv(),
+        ]);
+    }
 }
--- src/Handler/CsvResultHandler.php
+++ src/Handler/CsvResultHandler.php
@@ -2,7 +2,24 @@
 
 namespace App\Handler;
 
+/**
+ * Class CsvResultHandler
+ * @package App\Handler
+ */
+
 class CsvResultHandler
 {
     // Dans cette classe on gérera la lecture du fichier CSV de résultats à l'EuroMillions
+
+    function lireCsv()
+    {
+        $csv = new \SplFileObject('storage/euromillions.csv','r');
+
+        foreach($csv as $ligne) {
+            // on passe la ligne d'entete
+            return $ligne[1];
+        }
+    }
+
+
 }
